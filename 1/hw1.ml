3c2f624386d786b0c27826f48e303b28b2305bb2
BUNDA <classbun@lnxsrv11.seas.ucla.edu>
2024-01-20 10:36:48 -0800

    first commit

---
+++ 1/hw1.ml
@@ -0,0 +1,18 @@
+ (* Define a function to add two numbers *)
+let add a b = a + b;;
+
+(* Define a function to multiply two numbers *)
+let multiply a b = a * b;;
+
+(* Define a function that takes a function and two integers as arguments, 
+   and applies the function to the integers *)
+let operate func x y = func x y;;
+
+(* Use the 'operate' function with 'add' and 'multiply' *)
+let result1 = operate add 10 20;;
+let result2 = operate multiply 10 20;;
+
+(* 1. The function (subset a b) returns true iff a is a subset of b. Note that every set is a subset of itself. 
+   The function should apply to all lists. The function should also be curried.
+*)
+
